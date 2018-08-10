96b9eca6274155251b701ff04969800ffb1ca019
thomas <atelier@anybodesign.com>
2018-08-10 10:25:25 +0200

    page section

--- template-parts/page-section.php
+++ template-parts/page-section.php
@@ -10,18 +10,29 @@
  * @version 1.0
  */
 ?>
-					<div class="page-content">
+		<?php if ( '' != get_the_post_thumbnail() ) {
+			$img_url = wp_get_attachment_image_src( get_post_thumbnail_id(), 'large-hd' );
+			$bg = ' style="background-image: url('.$img_url[0].')"';
+		} else {
+			$bg = null;	
+		} ?>
+
+					<div class="page-section"<?php echo $bg; ?>>
 						
-						<h1 class="page-title"><?php the_title(); ?></h1>
-						
-						<div class="post-content">
-							<?php the_content(); ?>
+						<div class="inner">
+							
+							<?php if ( get_theme_mod('onepage') == true ) {
+								$h = 'h2';
+							} else {
+								$h = 'h1';	
+							} ?>
+							
+							<<?php echo $h; ?> class="page-title"><?php the_title(); ?></<?php echo $h; ?>>
+
+							<div class="page-content">
+								<?php the_content(); ?>
+							</div>
+							
 						</div>
 
-						<?php if ( '' != get_the_post_thumbnail() ) { ?>
-						<figure class="post-figure">
-							<a href="<?php the_permalink(); ?>"><?php the_post_thumbnail('large'); ?></a>
-						</figure>
-						<?php } ?>
-												
 					</div>
