a9d8001fd7a7d54ea6475fdc6248c19eeed90482
thomas <atelier@anybodesign.com>
2018-10-03 16:28:32 +0200

    Standalone template

---
+++ pagecustom-standalone.php
@@ -0,0 +1,10 @@
+<?php if ( !defined('ABSPATH') ) die();
+/**
+ * Template Name: Standalone Page
+ *
+ * @package WordPress
+ * @subpackage FS_Blocks
+ * @since 1.0
+ * @version 1.0
+ */
+get_template_part( 'page.php' ); ?>
