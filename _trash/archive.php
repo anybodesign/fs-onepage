d75edec5b003cf3eb217269243fc69a39e825300
Anybodesign <atelier@anybodesign.com>
2019-01-12 14:15:18 +0100

    Trash folder

---
+++ _trash/archive.php
@@ -0,0 +1,63 @@
+<?php if ( !defined('ABSPATH') ) die();
+/**
+ * The template for displaying archive pages.
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WordPress
+ * @subpackage FS_Blocks
+ * @since 1.0
+ * @version 1.0
+ */
+
+get_header(); ?>
+
+					<div class="archive-wrap has-sidebar">
+		
+						<?php if ( have_posts() ) : ?>
+						
+						<div class="archive-posts">
+
+							<?php get_sidebar(); ?>
+							
+							<div class="archive-posts-content">
+							
+								<header class="page-header">
+									<?php if ( is_archive() ) { 
+										the_archive_title( '<h1 class="page-title">', '</h1>' ); 
+										the_archive_description( '<div class="taxonomy-desc">', '</div>' ); 
+									} else { 
+										single_post_title('<h1>','</h1>'); 
+									} ?>
+								</header>				
+	
+								<div class="the-regular-posts">
+									<div class="the-posts">
+									
+									<?php while (have_posts()) : the_post();
+										
+										get_template_part( 'template-parts/post', 'block' );
+									
+									endwhile; wp_reset_postdata(); ?>
+	
+									</div>
+								</div>
+					
+								<?php the_posts_navigation(); ?>
+								
+							</div>
+							
+						</div>
+						
+						<?php else : ?>
+						
+						<div class="archive-posts">
+							<?php get_template_part( 'template-parts/nothing' ); ?>
+						</div>
+						
+						<?php endif; ?>	
+											
+					</div>
+					
+				
+<?php get_footer(); ?>
---
+++ _trash/search.php
@@ -0,0 +1,45 @@
+<?php if ( !defined('ABSPATH') ) die();
+/**
+ * The template for displaying Search Results pages.
+ *
+ * @package WordPress
+ * @subpackage FS_Blocks
+ * @since 1.0
+ * @version 1.0
+ */
+get_header(); ?>
+
+				<div class="row">
+					
+					<div class="col-9">
+	
+					<?php if ( have_posts() ) : ?>
+			
+						<header class="page-header">
+							<h1 class="page-title"><?php printf( esc_html__( 'Search Results for: %s', 'fs-blocks' ), '<span>' . get_search_query() . '</span>' ); ?></h1>
+						</header>				
+			
+						<?php while ( have_posts() ) : the_post(); ?>
+			
+							<?php get_template_part( 'template-parts/search', 'content' ); ?>
+			
+						<?php endwhile; ?>
+			
+						<?php the_posts_navigation(); ?>
+			
+					<?php else : ?>
+	
+						<?php get_template_part( 'template-parts/nothing' ); ?>
+				
+					<?php endif; ?>	
+						
+	
+					</div>
+
+					<div class="col-3">
+						<?php get_sidebar(); ?>
+					</div>					
+										
+				</div>
+			
+<?php get_footer(); ?>
