b1b33b3b292169aaf7bbda49b87b8bc166aa599a
Thomas Villain <atelier@anybodesign.com>
2020-04-14 22:41:30 +0200

    Page banner titles

--- index.php
+++ index.php
@@ -9,8 +9,13 @@
  * @since 1.0
  * @version 1.0
  */
-get_header();
-	
-			get_template_part( 'template-parts/post', 'frontpage' );
-
-get_footer(); ?>
+get_header(); ?>
+			
+				<div class="page-wrap">
+				<?php 
+					get_template_part( 'template-parts/page', 'banner' ); 
+					get_template_part( 'template-parts/post', 'frontpage' ); 
+				?>
+				</div>
+								
+<?php get_footer(); ?>
--- template-parts/page-banner.php
+++ template-parts/page-banner.php
@@ -8,7 +8,19 @@
 ?>
 						<div class="page-banner" data-scroll <?php fs_bg_img(); ?>>
 							<h1 class="page-title">
-								<?php the_title(); ?>
+							<?php 	
+								if ( is_home() && ! is_front_page() ) {
+									single_post_title();
+								} else if ( is_archive() ) {
+									the_archive_title();
+								} else if ( is_search() ) {
+									printf( esc_html__( 'Search Results for: %s', 'fs-onepage' ), '<span class="search-term">' . get_search_query() . '</span>' );
+								} else if ( is_404() ) {
+									esc_html_e( 'Oops! That page can&rsquo;t be found', 'fs-onepage' );
+								} else {
+									the_title(); 
+								}
+							?>
 							</h1>
 							<?php if ( is_single() ) {
 								get_template_part('template-parts/post', 'meta'); 
